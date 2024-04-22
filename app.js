62e11382555ebebd2f808e32c6a55c44bf35b47a
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 20:49:24 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // write code here
+function calculateCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+let radius = prompt("โปรดใส่ค่ารัศมีของวงกลม:");
+radius = parseFloat(radius);
+
+
+if (isNaN(radius) || radius <= 0) {
+  console.log("โปรดใส่ค่ารัศมีที่ถูกต้อง (ตัวเลขบวก)");
+} else {
+  
+  let circumference = calculateCircumference(radius);
+  console.log("ความยาวเส้นรอบวงของวงกลม:", circumference);
+}
